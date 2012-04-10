d36ea96f76b7f08a62897e0664c986e0adace05b
Marco Antonio Via <mvia@bcp.com.pe>
2012-04-10 00:14:21 -0500

    git@github.com:sblimper/FactoresPrimos.git

--- prime_factors_spec.rb
+++ prime_factors_spec.rb
@@ -1,8 +1,8 @@
-require "./PrimeFactors.rb"
+require "./prime_factors.rb"
 
 describe PrimeFactors do 
 
-	it "Obteniendo factores" do
+	it "LOS FACTORES SON" do
 		factores = prime_factors.new
 		factores.generate(16).should == [2,2,2,2] 
 	end
